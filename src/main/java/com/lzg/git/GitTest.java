aa5691ea8d04e3275f84385e9d9856bdef631982
wangxm <lizhigao521123@163.com>
2022-10-15 22:07:43 +0800

    hot-fix commit1

--- src/main/java/com/lzg/git/GitTest.java
+++ src/main/java/com/lzg/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("hello git4!");
+
     }
 }
