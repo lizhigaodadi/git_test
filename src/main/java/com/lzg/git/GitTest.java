23e9d157140887f614f61aada9227022c0bf3bc5
lizhigaodadi <96506885+lizhigaodadi@users.noreply.github.com>
2022-10-16 09:05:34 +0800

    Update GitTest.java
    test pull

--- src/main/java/com/lzg/git/GitTest.java
+++ src/main/java/com/lzg/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("master push!");
+        System.out.println("test pull!");
 
     }
 }
